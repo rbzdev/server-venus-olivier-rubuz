b53d74ee5a7849696463b4b41e6fdd0e0c344e6e
Olivier RUBUZ <rubuzolivier2@gmail.com>
2025-08-16 21:02:05 +0200

    Update README.md avec plus de details pour relancer facilement le projet

--- routes/articlesRoutes.js
+++ routes/articlesRoutes.js
@@ -6,11 +6,12 @@ const {
   reloadArticles
 } = require('../controllers/articlesController');
 
-// GET /api/articles
+// GET /api/articles 
 router.get('/', getAllArticles);
 
 // POST /api/articles  (AJouter un article depuis le frontend)
 router.post('/create', createArticle);
+
 
 // POST /api/articles/reload (pour recharger le fichier en mémoire du serveur ave fs)
 router.post('/reload', reloadArticles);
